554102cc146fa73d595a47490dd53de7e5a1fe65
SirFilipVonHabsburg <filip.boboli@gmail.com>
2025-12-11 08:28:33 +0100

    Hopefully final save
    those that boomed have been cooked

--- Assets/Enemy_Projectile.cs
+++ Assets/Enemy_Projectile.cs
@@ -1,14 +1,13 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Enemy_Projectile : MonoBehaviour
 {
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
-
+ 
     }
 
     // Update is called once per frame
@@ -19,9 +18,14 @@ public class Enemy_Projectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag== "Boundary")
+        Debug.Log("Destroyed by: " + collision.gameObject.name);
+
+
+        if (collision.gameObject.tag== "Boundary")
+           
         {
             Destroy(gameObject);
+            Debug.Log("Enemy Projectile");
         }
     }
 }
--- Assets/ProjectileSpawn.cs
+++ Assets/ProjectileSpawn.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class ProjectileSpawn : MonoBehaviour
 {
-    public GameObject Enemy_Projectile;  
+    public GameObject Enemy_Projectile;
+    public float spawnTimer = 10;
+    public float spawnMax = 10;
+    public float spawnMin = 5;
   
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnTimer = Random.Range(spawnMin, spawnMax);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(Enemy_Projectile, transform.position, Quaternion.identity);
+        spawnTimer -= Time.deltaTime;
+        if(spawnTimer<= 0)
+        {
+            Instantiate(Enemy_Projectile, transform.position, Quaternion.identity);
+            spawnTimer = Random.Range(spawnMin, spawnMax);
+        }
+            
     }
 }
