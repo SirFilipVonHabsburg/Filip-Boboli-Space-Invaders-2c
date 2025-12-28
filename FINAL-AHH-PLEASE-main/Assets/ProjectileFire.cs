8840a680372f66aae59c7ac3491d256bf2ad6a41
SirFilipVonHabsburg <filip.boboli@gmail.com>
2025-12-28 18:42:48 +0100

    Revert "aa"
    This reverts commit 1bfbed82b340078d38781515ede5fd111b3ed9fa.

---
+++ FINAL-AHH-PLEASE-main/Assets/ProjectileFire.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ProjectileFire : MonoBehaviour
+{
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Debug.Log("Strzelam");
+        }
+     }
+}
